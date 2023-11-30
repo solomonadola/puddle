cd2194d6b13cc7751a69c96a5e590bc2adb5935e
solomonadola <solomonadola84@gmail.com>
2023-11-30 15:57:32 +0300

    Item app complete

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
+from .forms import NewItemForm
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
     
-    return render(request, 'item/detail.html/', {'item': item})
+    return render(request, 'item/detail.html/', {'item': item, 'related_items': related_items})
+
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+    form = NewItemForm()
+    
+    return render(request, 'item/form.html', {'form': form})
+    
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -19,11 +19,9 @@ from django.urls import path, include
 from core.views import index, contact
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import index, contact
 
 urlpatterns = [
-    path('contact/',contact, name='contact'),
+    path('',include('core.urls')),
     path('items/', include('item.urls')),
-    path('',index, name="index"),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
